e698e93d75e471bd500dbd8f0dd0d1a2080e3a35
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2023-12-11 17:21:49 -0300

    first commit

---
+++ src/app/componentes/pensamentos/editar-pensamentos/editar-pensamentos.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-pensamentos',
+  templateUrl: './editar-pensamentos.component.html',
+  styleUrls: ['./editar-pensamentos.component.css']
+})
+export class EditarPensamentosComponent implements OnInit {
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1',
+    id: 0,
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute,
+
+    ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => this.pensamento=pensamento);
+  }
+
+  editarPensamento(){
+    this.service.editar(this.pensamento).subscribe(()=>{
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
+}
